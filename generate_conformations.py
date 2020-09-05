6a08c174a51a2ce8d0b0928959c8d4d220f871dc
Corin Wagen <corin.wagen@gmail.com>
2020-09-05 16:37:12 -0400

    make better

--- generate_conformations.py
+++ generate_conformations.py
@@ -6,19 +6,18 @@ import numpy as np
 parser = argparse.ArgumentParser(prog="generate_conformations.py")
 parser.add_argument("--procs", "-p", type=int, default=16, help="Number of processors to use.")
 parser.add_argument('-c', '--constraints', nargs='+', type=int, help="Atom numbers to freeze.")
+parser.add_argument('-l', '--logfile', type=str, default="crest.out", help="Logfile to write output to.")
+parser.add_argument('--noncovalent', action="store_true", help="Sets parameters to ``noncovalent`` in crest.")
 parser.add_argument("file", help="Molecule to perform conformation search on.")
 parser.add_argument("new_filename", help="Prefix for generated pairs.")
 
 args = vars(parser.parse_args(sys.argv[1:]))
 args["new_filename"] = re.sub(".gjf$", "", args["new_filename"])
 
-print("freezing")
-print(args["constraints"])
-
 f = cctk.GaussianFile.read_file(args["file"])
 molecule = f.get_molecule()
 
-e = molecule.csearch(nprocs=args["procs"], constraints=args["constraints"])
+e = molecule.csearch(nprocs=args["procs"], constraints=args["constraints"], logfile=args["logfile"], noncovalent=args["noncovalent"])
 
 for i, m in enumerate(e.molecule_list()):
     f.write_file(f"{args['new_filename']}_c{i:03d}.gjf", m)
